b64ee3bf9198262eea48d9d293119b9ce2996519
Dking08 <dastageer.siddiqui@gmail.com>
2026-03-16 14:32:08 +0530

    feat(db): add prisma, postgresql setup

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import express, { Request, Response } from 'express';
+import { prisma } from "./prisma";
+import { LinkPrecedence } from './generated/prisma/enums';
+import 'dotenv/config'
 
 const app = express();
 const port = 8001;
@@ -9,19 +12,34 @@ interface identifyR {
   phoneNumber?: number
 }
 
-app.post('/identify', (req: Request<{}, {}, identifyR>, res: Response) => {
+app.post('/identify', async (req: Request<{}, {}, identifyR>, res: Response) => {
   const { email, phoneNumber } = req.body;
   if (!email && !phoneNumber) {
     return res.status(400).json({
       error: "email or phoneNumber required!"
     });
   }
+  // console.log(process.env.DATABASE_URL);
+  const contact = await prisma.contact.create({
+    data: {
+      email,
+      phoneNumber,
+      linkPrecedence: "primary"
+    }
+  });
   console.log("Request: ", req.body);
   res.json({
     message: "This is a responce",
     email,
-    phoneNumber
+    phoneNumber,
+    contact: contact
   });
+});
+
+// ** TESTING **
+app.get('/api/debug/get-db', async (req: Request, res: Response) => {
+  const contact = await prisma.contact.findMany();
+  res.json(contact);
 });
 
 app.listen(port, () => {
